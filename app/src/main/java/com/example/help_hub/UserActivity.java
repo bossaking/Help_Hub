d3418143bc02248565e8604fb0244df7fd100678
Gabrielos1 <73485727+Gabrielos1@users.noreply.github.com>
2020-10-29 14:19:14 +0100

    Dodany widok edycji dannych użytkownika
    Dodany widok edycji dannych użytkownika

--- app/src/main/java/com/example/help_hub/UserActivity.java
+++ app/src/main/java/com/example/help_hub/UserActivity.java
@@ -2,7 +2,10 @@ package com.example.help_hub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        Button editButton = findViewById(R.id.user_edit_button);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),UserDataChange.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

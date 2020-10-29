78957a1b6aaf6ea9449aca05701381c2f1fbd536
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2020-10-29 16:15:00 +0100

    Zmiana awatara
    Dodano możliwość zmiany awatara na wybrany z galerii

--- app/src/main/java/com/example/help_hub/UserActivity.java
+++ app/src/main/java/com/example/help_hub/UserActivity.java
@@ -1,18 +1,41 @@
 package com.example.help_hub;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class UserActivity extends AppCompatActivity {
+
+    private ImageView ProfileImage;
+    private static final int PICK_IMAGE = 1;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        ProfileImage = (ImageView) findViewById(R.id.Profile_Image);
+        ProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(gallery, "Sellect Picture"), PICK_IMAGE);
+            }
+        });
 
         Button editButton = findViewById(R.id.user_edit_button);
         editButton.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +45,20 @@ public class UserActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                ProfileImage.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }

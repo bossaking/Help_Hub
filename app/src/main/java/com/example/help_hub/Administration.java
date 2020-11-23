bd743c299cf348dd06fcd3e7b3243dbe02c6c7fc
Matthew789123 <Matthew789123@gmail.com>
2020-11-23 14:34:07 +0100

    Revert "adding forbidden words"
    This reverts commit 251ad91718d8b7acf33cb1c206a672d643b148c4.

--- app/src/main/java/com/example/help_hub/Administration.java
+++ app/src/main/java/com/example/help_hub/Administration.java
@@ -1,6 +1,5 @@
 package com.example.help_hub;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,7 +7,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,14 +59,6 @@ public class Administration extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_administration, container, false);
-
-        Button forbidden_words_list_button = view.findViewById(R.id.forbidden_words_list_button);
-
-        forbidden_words_list_button.setOnClickListener(v -> {
-            startActivity(new Intent(getActivity().getApplicationContext(), ForbiddenWordsActivity.class));
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_administration, container, false);
     }
 }

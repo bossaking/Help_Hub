5a2026bb5ee149a77079eafa9c526c7bee465f43
Matthew789123 <Matthew789123@gmail.com>
2020-12-17 21:27:40 +0100

    All Need Help

---
+++ app/src/main/java/com/example/help_hub/OtherClasses/Order.java
@@ -0,0 +1,43 @@
+package com.example.help_hub.OtherClasses;
+
+import android.net.Uri;
+
+public class Order {
+
+    private String Title, Price = "30", Description, Id;
+
+    public Order() {
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        Price = price;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+}

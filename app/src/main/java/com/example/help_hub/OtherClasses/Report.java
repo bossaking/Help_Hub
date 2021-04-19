703793e9009219b8b45cfac19e0a5816f781ba1e
Dymitr <54572948+bossaking@users.noreply.github.com>
2021-04-19 16:00:18 +0200

    Dodano możliwość zgłoszenia oferty lub ogłoszenia do administratora.

---
+++ app/src/main/java/com/example/help_hub/OtherClasses/Report.java
@@ -0,0 +1,34 @@
+package com.example.help_hub.OtherClasses;
+
+public class Report {
+
+    private String ReportID, Cause, UserID;
+
+    public Report(){
+
+    }
+
+    public String getReportID() {
+        return ReportID;
+    }
+
+    public void setReportID(String ReportID) {
+        this.ReportID = ReportID;
+    }
+
+    public String getCause() {
+        return Cause;
+    }
+
+    public void setCause(String cause) {
+        Cause = cause;
+    }
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(String userID) {
+        UserID = userID;
+    }
+}

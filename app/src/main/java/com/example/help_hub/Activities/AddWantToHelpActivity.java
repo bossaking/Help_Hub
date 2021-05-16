49ef8a8bdec6c3bc58ee4021acea6817ae46aa41
Gabrielos1 <73485727+Gabrielos1@users.noreply.github.com>
2021-05-16 19:00:22 +0200

    AddWantToHelpActivity reviewed
    AddWantToHelpActivity reviewed
    
    Co-Authored-By: Mateusz Paszko <73493587+nieswietymikolaj@users.noreply.github.com>

--- app/src/main/java/com/example/help_hub/Activities/AddWantToHelpActivity.java
+++ app/src/main/java/com/example/help_hub/Activities/AddWantToHelpActivity.java
@@ -3,6 +3,8 @@ package com.example.help_hub.Activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
@@ -22,19 +24,18 @@ import java.util.Map;
 
 public class AddWantToHelpActivity extends NewOfferNoticeCategory implements TextWatcher {
 
-    private EditText mNewOfferTitle;
-    private EditText mNewOfferDescription;
-    private EditText needHelpPrice;
-    private Button addNewOfferButton, categoriesButton;
-
-    private FirebaseFirestore firebaseFirestore;
-    FirebaseAuth firebaseAuth;
-
-    String userId;
-    String userCity;
-
+    private EditText wantToHelpTitle, wantToHelpDescription, wantToHelpPrice;
+    private Button addButton, categoriesButton;
     private Drawable defaultBackground;
 
+    private String userId, userCity;
+
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+
+    private Context context;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,90 +49,89 @@ public class AddWantToHelpActivity extends NewOfferNoticeCategory implements Tex
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        context = getApplicationContext();
+
         firebaseFirestore = FirebaseFirestore.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
 
         userId = firebaseAuth.getUid();
+
         firebaseFirestore.collection("users").document(userId).get().addOnCompleteListener(task -> {
-            if(task.isSuccessful()){
+            if (task.isSuccessful()) {
                 userCity = task.getResult().getString("City");
                 loadingDialog.DismissDialog();
             }
         });
 
-        mNewOfferTitle = findViewById(R.id.new_offer_title_edit_text);
-        needHelpPrice = findViewById(R.id.new_offer_cost);
-        mNewOfferDescription = findViewById(R.id.new_offer_description_edit_text);
+        wantToHelpTitle = findViewById(R.id.new_offer_title_edit_text);
+        wantToHelpPrice = findViewById(R.id.new_offer_cost);
+        wantToHelpDescription = findViewById(R.id.new_offer_description_edit_text);
 
-        defaultBackground = mNewOfferTitle.getBackground();
-        mNewOfferTitle.addTextChangedListener(this);
+        defaultBackground = wantToHelpTitle.getBackground();
+        wantToHelpTitle.addTextChangedListener(this);
+
+        addButton = findViewById(R.id.new_offer_add_offer_button);
+        addButton.setOnClickListener(v -> {
+            title = wantToHelpTitle.getText().toString().trim();
+            price = wantToHelpPrice.getText().toString().trim();
+            description = wantToHelpDescription.getText().toString().trim();
+            addNewWantToHelp();
+        });
 
         categoriesButton = findViewById(R.id.new_offer_select_category_button);
         categoriesButton.setOnClickListener(v -> SelectCategory());
-        SetOnTitleChangedListener(() -> {
-            categoriesButton.setText(categoryTitle + " / " + subCategoryTitle);
-        });
-
-        addNewOfferButton = findViewById(R.id.new_offer_add_offer_button);
-        addNewOfferButton.setOnClickListener(v -> {
-            title = mNewOfferTitle.getText().toString().trim();
-            price = needHelpPrice.getText().toString().trim();
-            description = mNewOfferDescription.getText().toString().trim();
-            addNewOffer();
-        });
+        SetOnTitleChangedListener(() -> categoriesButton.setText(categoryTitle + " / " + subCategoryTitle));
     }
 
-    private void addNewOffer() {
-        if (!validateData() || !CheckForbiddenWords()) {
-            return;
-        }
-        Map<String, Object> offerMap = new HashMap<>();
-        offerMap.put("Title", title);
-        offerMap.put("Price", price);
-        offerMap.put("Description", description);
-        offerMap.put("Category", categoryTitle);
-        offerMap.put("Subcategory", subCategoryTitle);
-        offerMap.put("UserId", userId);
-        offerMap.put("ShowsCount", 0);
-        offerMap.put("City", userCity);
+    private void addNewWantToHelp() {
+        if (!validateData() || !CheckForbiddenWords()) return;
 
-        firebaseFirestore.collection("offers").document().set(offerMap).addOnSuccessListener(v -> {
-            Toast.makeText(getApplicationContext(), getString(R.string.success), Toast.LENGTH_SHORT).show();
+        Map<String, Object> wantToHelpMap = new HashMap<>();
+        wantToHelpMap.put("Title", title);
+        wantToHelpMap.put("Price", price);
+        wantToHelpMap.put("Description", description);
+        wantToHelpMap.put("Category", categoryTitle);
+        wantToHelpMap.put("Subcategory", subCategoryTitle);
+        wantToHelpMap.put("UserId", userId);
+        wantToHelpMap.put("ShowsCount", 0);
+        wantToHelpMap.put("City", userCity);
+
+        firebaseFirestore.collection("offers").document().set(wantToHelpMap).addOnSuccessListener(v -> {
+            Toast.makeText(context, getString(R.string.success), Toast.LENGTH_SHORT).show();
             finish();
-        }).addOnFailureListener(v -> Toast.makeText(getApplicationContext(), getString(R.string.error) + v.getLocalizedMessage(), Toast.LENGTH_LONG).show());
+        }).addOnFailureListener(v -> Toast.makeText(context, getString(R.string.error) + v.getLocalizedMessage(), Toast.LENGTH_LONG).show());
     }
 
     private boolean validateData() {
         if (title.isEmpty()) {
-            mNewOfferTitle.setBackgroundResource(R.drawable.edit_error_border);
-            mNewOfferTitle.setError(getString(R.string.empty_field_error));
+            wantToHelpTitle.setBackgroundResource(R.drawable.edit_error_border);
+            wantToHelpTitle.setError(getString(R.string.empty_field_error));
             return false;
         }
         if (price.isEmpty()) {
-            needHelpPrice.setBackgroundResource(R.drawable.edit_error_border);
-            needHelpPrice.setError(getString(R.string.empty_field_error));
+            wantToHelpPrice.setBackgroundResource(R.drawable.edit_error_border);
+            wantToHelpPrice.setError(getString(R.string.empty_field_error));
             return false;
         }
         if (categoryTitle.isEmpty()) {
             Toast.makeText(this, getString(R.string.empty_field_error), Toast.LENGTH_LONG).show();
             return false;
         }
+
         return true;
     }
 
     @Override
     public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
     }
 
     @Override
     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        mNewOfferTitle.setBackground(defaultBackground);
+        wantToHelpTitle.setBackground(defaultBackground);
     }
 
     @Override
     public void afterTextChanged(Editable editable) {
-
     }
 
     @Override
